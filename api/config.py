6cb382941a73a090fec388e73808a34734a9cb9c
Matumbai Binale <binalematumbai@gmail.com>
2024-10-23 19:33:20 +0300

    (feat): Configure swagger ui for documentation

--- api/config.py
+++ api/config.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///ishare.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
+    # SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
 
     # JWT Configuration
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt_secret_key')
